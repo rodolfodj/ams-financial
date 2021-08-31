37a4aa9f6ca25fde2fee2353a848ba21d8e618fe
Rodolfo <rodolfodj@gmail.com>
2021-08-30 22:12:57 -0300

    swagger

--- src/main/java/br/com/mesttra/financial/controller/FinancialController.java
+++ src/main/java/br/com/mesttra/financial/controller/FinancialController.java
@@ -4,20 +4,30 @@ package br.com.mesttra.financial.controller;
 import br.com.mesttra.financial.dto.PlayerDto;
 import br.com.mesttra.financial.entity.MonthlyExpense;
 import br.com.mesttra.financial.service.FinancialService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 @AllArgsConstructor
+@Api(value = "Financial", tags = {"Financial"})
 public class FinancialController {
 
     private final FinancialService service;
 
     @PostMapping("/hiring")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Contratar jogador",
+            notes = "This method hire a new player")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Expense added"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public MonthlyExpense hireNewPlayer(@RequestBody PlayerDto body) {
         return service.addNewPlayer(body);
     }
--- src/main/java/br/com/mesttra/financial/dto/PlayerDto.java
+++ src/main/java/br/com/mesttra/financial/dto/PlayerDto.java
@@ -1,10 +1,22 @@
 package br.com.mesttra.financial.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class PlayerDto {
 
+    @ApiModelProperty(
+            value = "Player name",
+            name = "name",
+            dataType = "String",
+            example = "João Almeida")
     private String name;
+
+    @ApiModelProperty(
+            value = "Player salary",
+            name = "salary",
+            dataType = "Double",
+            example = "500000")
     private Double salary;
 }
